d434731c6bd06cf55e255660402159f5daeb776d
m44328576-dotcom <m44328576@gmail.com>
2025-10-17 23:27:06 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,910 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Admin Panel v9"
+
+@app.route('/route1')
+def route_1():
+    return 'Route 1'
+@app.route('/route2')
+def route_2():
+    return 'Route 2'
+@app.route('/route3')
+def route_3():
+    return 'Route 3'
+@app.route('/route4')
+def route_4():
+    return 'Route 4'
+@app.route('/route5')
+def route_5():
+    return 'Route 5'
+@app.route('/route6')
+def route_6():
+    return 'Route 6'
+@app.route('/route7')
+def route_7():
+    return 'Route 7'
+@app.route('/route8')
+def route_8():
+    return 'Route 8'
+@app.route('/route9')
+def route_9():
+    return 'Route 9'
+@app.route('/route10')
+def route_10():
+    return 'Route 10'
+@app.route('/route11')
+def route_11():
+    return 'Route 11'
+@app.route('/route12')
+def route_12():
+    return 'Route 12'
+@app.route('/route13')
+def route_13():
+    return 'Route 13'
+@app.route('/route14')
+def route_14():
+    return 'Route 14'
+@app.route('/route15')
+def route_15():
+    return 'Route 15'
+@app.route('/route16')
+def route_16():
+    return 'Route 16'
+@app.route('/route17')
+def route_17():
+    return 'Route 17'
+@app.route('/route18')
+def route_18():
+    return 'Route 18'
+@app.route('/route19')
+def route_19():
+    return 'Route 19'
+@app.route('/route20')
+def route_20():
+    return 'Route 20'
+@app.route('/route21')
+def route_21():
+    return 'Route 21'
+@app.route('/route22')
+def route_22():
+    return 'Route 22'
+@app.route('/route23')
+def route_23():
+    return 'Route 23'
+@app.route('/route24')
+def route_24():
+    return 'Route 24'
+@app.route('/route25')
+def route_25():
+    return 'Route 25'
+@app.route('/route26')
+def route_26():
+    return 'Route 26'
+@app.route('/route27')
+def route_27():
+    return 'Route 27'
+@app.route('/route28')
+def route_28():
+    return 'Route 28'
+@app.route('/route29')
+def route_29():
+    return 'Route 29'
+@app.route('/route30')
+def route_30():
+    return 'Route 30'
+@app.route('/route31')
+def route_31():
+    return 'Route 31'
+@app.route('/route32')
+def route_32():
+    return 'Route 32'
+@app.route('/route33')
+def route_33():
+    return 'Route 33'
+@app.route('/route34')
+def route_34():
+    return 'Route 34'
+@app.route('/route35')
+def route_35():
+    return 'Route 35'
+@app.route('/route36')
+def route_36():
+    return 'Route 36'
+@app.route('/route37')
+def route_37():
+    return 'Route 37'
+@app.route('/route38')
+def route_38():
+    return 'Route 38'
+@app.route('/route39')
+def route_39():
+    return 'Route 39'
+@app.route('/route40')
+def route_40():
+    return 'Route 40'
+@app.route('/route41')
+def route_41():
+    return 'Route 41'
+@app.route('/route42')
+def route_42():
+    return 'Route 42'
+@app.route('/route43')
+def route_43():
+    return 'Route 43'
+@app.route('/route44')
+def route_44():
+    return 'Route 44'
+@app.route('/route45')
+def route_45():
+    return 'Route 45'
+@app.route('/route46')
+def route_46():
+    return 'Route 46'
+@app.route('/route47')
+def route_47():
+    return 'Route 47'
+@app.route('/route48')
+def route_48():
+    return 'Route 48'
+@app.route('/route49')
+def route_49():
+    return 'Route 49'
+@app.route('/route50')
+def route_50():
+    return 'Route 50'
+@app.route('/route51')
+def route_51():
+    return 'Route 51'
+@app.route('/route52')
+def route_52():
+    return 'Route 52'
+@app.route('/route53')
+def route_53():
+    return 'Route 53'
+@app.route('/route54')
+def route_54():
+    return 'Route 54'
+@app.route('/route55')
+def route_55():
+    return 'Route 55'
+@app.route('/route56')
+def route_56():
+    return 'Route 56'
+@app.route('/route57')
+def route_57():
+    return 'Route 57'
+@app.route('/route58')
+def route_58():
+    return 'Route 58'
+@app.route('/route59')
+def route_59():
+    return 'Route 59'
+@app.route('/route60')
+def route_60():
+    return 'Route 60'
+@app.route('/route61')
+def route_61():
+    return 'Route 61'
+@app.route('/route62')
+def route_62():
+    return 'Route 62'
+@app.route('/route63')
+def route_63():
+    return 'Route 63'
+@app.route('/route64')
+def route_64():
+    return 'Route 64'
+@app.route('/route65')
+def route_65():
+    return 'Route 65'
+@app.route('/route66')
+def route_66():
+    return 'Route 66'
+@app.route('/route67')
+def route_67():
+    return 'Route 67'
+@app.route('/route68')
+def route_68():
+    return 'Route 68'
+@app.route('/route69')
+def route_69():
+    return 'Route 69'
+@app.route('/route70')
+def route_70():
+    return 'Route 70'
+@app.route('/route71')
+def route_71():
+    return 'Route 71'
+@app.route('/route72')
+def route_72():
+    return 'Route 72'
+@app.route('/route73')
+def route_73():
+    return 'Route 73'
+@app.route('/route74')
+def route_74():
+    return 'Route 74'
+@app.route('/route75')
+def route_75():
+    return 'Route 75'
+@app.route('/route76')
+def route_76():
+    return 'Route 76'
+@app.route('/route77')
+def route_77():
+    return 'Route 77'
+@app.route('/route78')
+def route_78():
+    return 'Route 78'
+@app.route('/route79')
+def route_79():
+    return 'Route 79'
+@app.route('/route80')
+def route_80():
+    return 'Route 80'
+@app.route('/route81')
+def route_81():
+    return 'Route 81'
+@app.route('/route82')
+def route_82():
+    return 'Route 82'
+@app.route('/route83')
+def route_83():
+    return 'Route 83'
+@app.route('/route84')
+def route_84():
+    return 'Route 84'
+@app.route('/route85')
+def route_85():
+    return 'Route 85'
+@app.route('/route86')
+def route_86():
+    return 'Route 86'
+@app.route('/route87')
+def route_87():
+    return 'Route 87'
+@app.route('/route88')
+def route_88():
+    return 'Route 88'
+@app.route('/route89')
+def route_89():
+    return 'Route 89'
+@app.route('/route90')
+def route_90():
+    return 'Route 90'
+@app.route('/route91')
+def route_91():
+    return 'Route 91'
+@app.route('/route92')
+def route_92():
+    return 'Route 92'
+@app.route('/route93')
+def route_93():
+    return 'Route 93'
+@app.route('/route94')
+def route_94():
+    return 'Route 94'
+@app.route('/route95')
+def route_95():
+    return 'Route 95'
+@app.route('/route96')
+def route_96():
+    return 'Route 96'
+@app.route('/route97')
+def route_97():
+    return 'Route 97'
+@app.route('/route98')
+def route_98():
+    return 'Route 98'
+@app.route('/route99')
+def route_99():
+    return 'Route 99'
+@app.route('/route100')
+def route_100():
+    return 'Route 100'
+@app.route('/route101')
+def route_101():
+    return 'Route 101'
+@app.route('/route102')
+def route_102():
+    return 'Route 102'
+@app.route('/route103')
+def route_103():
+    return 'Route 103'
+@app.route('/route104')
+def route_104():
+    return 'Route 104'
+@app.route('/route105')
+def route_105():
+    return 'Route 105'
+@app.route('/route106')
+def route_106():
+    return 'Route 106'
+@app.route('/route107')
+def route_107():
+    return 'Route 107'
+@app.route('/route108')
+def route_108():
+    return 'Route 108'
+@app.route('/route109')
+def route_109():
+    return 'Route 109'
+@app.route('/route110')
+def route_110():
+    return 'Route 110'
+@app.route('/route111')
+def route_111():
+    return 'Route 111'
+@app.route('/route112')
+def route_112():
+    return 'Route 112'
+@app.route('/route113')
+def route_113():
+    return 'Route 113'
+@app.route('/route114')
+def route_114():
+    return 'Route 114'
+@app.route('/route115')
+def route_115():
+    return 'Route 115'
+@app.route('/route116')
+def route_116():
+    return 'Route 116'
+@app.route('/route117')
+def route_117():
+    return 'Route 117'
+@app.route('/route118')
+def route_118():
+    return 'Route 118'
+@app.route('/route119')
+def route_119():
+    return 'Route 119'
+@app.route('/route120')
+def route_120():
+    return 'Route 120'
+@app.route('/route121')
+def route_121():
+    return 'Route 121'
+@app.route('/route122')
+def route_122():
+    return 'Route 122'
+@app.route('/route123')
+def route_123():
+    return 'Route 123'
+@app.route('/route124')
+def route_124():
+    return 'Route 124'
+@app.route('/route125')
+def route_125():
+    return 'Route 125'
+@app.route('/route126')
+def route_126():
+    return 'Route 126'
+@app.route('/route127')
+def route_127():
+    return 'Route 127'
+@app.route('/route128')
+def route_128():
+    return 'Route 128'
+@app.route('/route129')
+def route_129():
+    return 'Route 129'
+@app.route('/route130')
+def route_130():
+    return 'Route 130'
+@app.route('/route131')
+def route_131():
+    return 'Route 131'
+@app.route('/route132')
+def route_132():
+    return 'Route 132'
+@app.route('/route133')
+def route_133():
+    return 'Route 133'
+@app.route('/route134')
+def route_134():
+    return 'Route 134'
+@app.route('/route135')
+def route_135():
+    return 'Route 135'
+@app.route('/route136')
+def route_136():
+    return 'Route 136'
+@app.route('/route137')
+def route_137():
+    return 'Route 137'
+@app.route('/route138')
+def route_138():
+    return 'Route 138'
+@app.route('/route139')
+def route_139():
+    return 'Route 139'
+@app.route('/route140')
+def route_140():
+    return 'Route 140'
+@app.route('/route141')
+def route_141():
+    return 'Route 141'
+@app.route('/route142')
+def route_142():
+    return 'Route 142'
+@app.route('/route143')
+def route_143():
+    return 'Route 143'
+@app.route('/route144')
+def route_144():
+    return 'Route 144'
+@app.route('/route145')
+def route_145():
+    return 'Route 145'
+@app.route('/route146')
+def route_146():
+    return 'Route 146'
+@app.route('/route147')
+def route_147():
+    return 'Route 147'
+@app.route('/route148')
+def route_148():
+    return 'Route 148'
+@app.route('/route149')
+def route_149():
+    return 'Route 149'
+@app.route('/route150')
+def route_150():
+    return 'Route 150'
+@app.route('/route151')
+def route_151():
+    return 'Route 151'
+@app.route('/route152')
+def route_152():
+    return 'Route 152'
+@app.route('/route153')
+def route_153():
+    return 'Route 153'
+@app.route('/route154')
+def route_154():
+    return 'Route 154'
+@app.route('/route155')
+def route_155():
+    return 'Route 155'
+@app.route('/route156')
+def route_156():
+    return 'Route 156'
+@app.route('/route157')
+def route_157():
+    return 'Route 157'
+@app.route('/route158')
+def route_158():
+    return 'Route 158'
+@app.route('/route159')
+def route_159():
+    return 'Route 159'
+@app.route('/route160')
+def route_160():
+    return 'Route 160'
+@app.route('/route161')
+def route_161():
+    return 'Route 161'
+@app.route('/route162')
+def route_162():
+    return 'Route 162'
+@app.route('/route163')
+def route_163():
+    return 'Route 163'
+@app.route('/route164')
+def route_164():
+    return 'Route 164'
+@app.route('/route165')
+def route_165():
+    return 'Route 165'
+@app.route('/route166')
+def route_166():
+    return 'Route 166'
+@app.route('/route167')
+def route_167():
+    return 'Route 167'
+@app.route('/route168')
+def route_168():
+    return 'Route 168'
+@app.route('/route169')
+def route_169():
+    return 'Route 169'
+@app.route('/route170')
+def route_170():
+    return 'Route 170'
+@app.route('/route171')
+def route_171():
+    return 'Route 171'
+@app.route('/route172')
+def route_172():
+    return 'Route 172'
+@app.route('/route173')
+def route_173():
+    return 'Route 173'
+@app.route('/route174')
+def route_174():
+    return 'Route 174'
+@app.route('/route175')
+def route_175():
+    return 'Route 175'
+@app.route('/route176')
+def route_176():
+    return 'Route 176'
+@app.route('/route177')
+def route_177():
+    return 'Route 177'
+@app.route('/route178')
+def route_178():
+    return 'Route 178'
+@app.route('/route179')
+def route_179():
+    return 'Route 179'
+@app.route('/route180')
+def route_180():
+    return 'Route 180'
+@app.route('/route181')
+def route_181():
+    return 'Route 181'
+@app.route('/route182')
+def route_182():
+    return 'Route 182'
+@app.route('/route183')
+def route_183():
+    return 'Route 183'
+@app.route('/route184')
+def route_184():
+    return 'Route 184'
+@app.route('/route185')
+def route_185():
+    return 'Route 185'
+@app.route('/route186')
+def route_186():
+    return 'Route 186'
+@app.route('/route187')
+def route_187():
+    return 'Route 187'
+@app.route('/route188')
+def route_188():
+    return 'Route 188'
+@app.route('/route189')
+def route_189():
+    return 'Route 189'
+@app.route('/route190')
+def route_190():
+    return 'Route 190'
+@app.route('/route191')
+def route_191():
+    return 'Route 191'
+@app.route('/route192')
+def route_192():
+    return 'Route 192'
+@app.route('/route193')
+def route_193():
+    return 'Route 193'
+@app.route('/route194')
+def route_194():
+    return 'Route 194'
+@app.route('/route195')
+def route_195():
+    return 'Route 195'
+@app.route('/route196')
+def route_196():
+    return 'Route 196'
+@app.route('/route197')
+def route_197():
+    return 'Route 197'
+@app.route('/route198')
+def route_198():
+    return 'Route 198'
+@app.route('/route199')
+def route_199():
+    return 'Route 199'
+@app.route('/route200')
+def route_200():
+    return 'Route 200'
+@app.route('/route201')
+def route_201():
+    return 'Route 201'
+@app.route('/route202')
+def route_202():
+    return 'Route 202'
+@app.route('/route203')
+def route_203():
+    return 'Route 203'
+@app.route('/route204')
+def route_204():
+    return 'Route 204'
+@app.route('/route205')
+def route_205():
+    return 'Route 205'
+@app.route('/route206')
+def route_206():
+    return 'Route 206'
+@app.route('/route207')
+def route_207():
+    return 'Route 207'
+@app.route('/route208')
+def route_208():
+    return 'Route 208'
+@app.route('/route209')
+def route_209():
+    return 'Route 209'
+@app.route('/route210')
+def route_210():
+    return 'Route 210'
+@app.route('/route211')
+def route_211():
+    return 'Route 211'
+@app.route('/route212')
+def route_212():
+    return 'Route 212'
+@app.route('/route213')
+def route_213():
+    return 'Route 213'
+@app.route('/route214')
+def route_214():
+    return 'Route 214'
+@app.route('/route215')
+def route_215():
+    return 'Route 215'
+@app.route('/route216')
+def route_216():
+    return 'Route 216'
+@app.route('/route217')
+def route_217():
+    return 'Route 217'
+@app.route('/route218')
+def route_218():
+    return 'Route 218'
+@app.route('/route219')
+def route_219():
+    return 'Route 219'
+@app.route('/route220')
+def route_220():
+    return 'Route 220'
+@app.route('/route221')
+def route_221():
+    return 'Route 221'
+@app.route('/route222')
+def route_222():
+    return 'Route 222'
+@app.route('/route223')
+def route_223():
+    return 'Route 223'
+@app.route('/route224')
+def route_224():
+    return 'Route 224'
+@app.route('/route225')
+def route_225():
+    return 'Route 225'
+@app.route('/route226')
+def route_226():
+    return 'Route 226'
+@app.route('/route227')
+def route_227():
+    return 'Route 227'
+@app.route('/route228')
+def route_228():
+    return 'Route 228'
+@app.route('/route229')
+def route_229():
+    return 'Route 229'
+@app.route('/route230')
+def route_230():
+    return 'Route 230'
+@app.route('/route231')
+def route_231():
+    return 'Route 231'
+@app.route('/route232')
+def route_232():
+    return 'Route 232'
+@app.route('/route233')
+def route_233():
+    return 'Route 233'
+@app.route('/route234')
+def route_234():
+    return 'Route 234'
+@app.route('/route235')
+def route_235():
+    return 'Route 235'
+@app.route('/route236')
+def route_236():
+    return 'Route 236'
+@app.route('/route237')
+def route_237():
+    return 'Route 237'
+@app.route('/route238')
+def route_238():
+    return 'Route 238'
+@app.route('/route239')
+def route_239():
+    return 'Route 239'
+@app.route('/route240')
+def route_240():
+    return 'Route 240'
+@app.route('/route241')
+def route_241():
+    return 'Route 241'
+@app.route('/route242')
+def route_242():
+    return 'Route 242'
+@app.route('/route243')
+def route_243():
+    return 'Route 243'
+@app.route('/route244')
+def route_244():
+    return 'Route 244'
+@app.route('/route245')
+def route_245():
+    return 'Route 245'
+@app.route('/route246')
+def route_246():
+    return 'Route 246'
+@app.route('/route247')
+def route_247():
+    return 'Route 247'
+@app.route('/route248')
+def route_248():
+    return 'Route 248'
+@app.route('/route249')
+def route_249():
+    return 'Route 249'
+@app.route('/route250')
+def route_250():
+    return 'Route 250'
+@app.route('/route251')
+def route_251():
+    return 'Route 251'
+@app.route('/route252')
+def route_252():
+    return 'Route 252'
+@app.route('/route253')
+def route_253():
+    return 'Route 253'
+@app.route('/route254')
+def route_254():
+    return 'Route 254'
+@app.route('/route255')
+def route_255():
+    return 'Route 255'
+@app.route('/route256')
+def route_256():
+    return 'Route 256'
+@app.route('/route257')
+def route_257():
+    return 'Route 257'
+@app.route('/route258')
+def route_258():
+    return 'Route 258'
+@app.route('/route259')
+def route_259():
+    return 'Route 259'
+@app.route('/route260')
+def route_260():
+    return 'Route 260'
+@app.route('/route261')
+def route_261():
+    return 'Route 261'
+@app.route('/route262')
+def route_262():
+    return 'Route 262'
+@app.route('/route263')
+def route_263():
+    return 'Route 263'
+@app.route('/route264')
+def route_264():
+    return 'Route 264'
+@app.route('/route265')
+def route_265():
+    return 'Route 265'
+@app.route('/route266')
+def route_266():
+    return 'Route 266'
+@app.route('/route267')
+def route_267():
+    return 'Route 267'
+@app.route('/route268')
+def route_268():
+    return 'Route 268'
+@app.route('/route269')
+def route_269():
+    return 'Route 269'
+@app.route('/route270')
+def route_270():
+    return 'Route 270'
+@app.route('/route271')
+def route_271():
+    return 'Route 271'
+@app.route('/route272')
+def route_272():
+    return 'Route 272'
+@app.route('/route273')
+def route_273():
+    return 'Route 273'
+@app.route('/route274')
+def route_274():
+    return 'Route 274'
+@app.route('/route275')
+def route_275():
+    return 'Route 275'
+@app.route('/route276')
+def route_276():
+    return 'Route 276'
+@app.route('/route277')
+def route_277():
+    return 'Route 277'
+@app.route('/route278')
+def route_278():
+    return 'Route 278'
+@app.route('/route279')
+def route_279():
+    return 'Route 279'
+@app.route('/route280')
+def route_280():
+    return 'Route 280'
+@app.route('/route281')
+def route_281():
+    return 'Route 281'
+@app.route('/route282')
+def route_282():
+    return 'Route 282'
+@app.route('/route283')
+def route_283():
+    return 'Route 283'
+@app.route('/route284')
+def route_284():
+    return 'Route 284'
+@app.route('/route285')
+def route_285():
+    return 'Route 285'
+@app.route('/route286')
+def route_286():
+    return 'Route 286'
+@app.route('/route287')
+def route_287():
+    return 'Route 287'
+@app.route('/route288')
+def route_288():
+    return 'Route 288'
+@app.route('/route289')
+def route_289():
+    return 'Route 289'
+@app.route('/route290')
+def route_290():
+    return 'Route 290'
+@app.route('/route291')
+def route_291():
+    return 'Route 291'
+@app.route('/route292')
+def route_292():
+    return 'Route 292'
+@app.route('/route293')
+def route_293():
+    return 'Route 293'
+@app.route('/route294')
+def route_294():
+    return 'Route 294'
+@app.route('/route295')
+def route_295():
+    return 'Route 295'
+@app.route('/route296')
+def route_296():
+    return 'Route 296'
+@app.route('/route297')
+def route_297():
+    return 'Route 297'
+@app.route('/route298')
+def route_298():
+    return 'Route 298'
+@app.route('/route299')
+def route_299():
+    return 'Route 299'
+@app.route('/route300')
+def route_300():
+    return 'Route 300'
+
+if __name__ == '__main__':
+    app.run(port=5000)
